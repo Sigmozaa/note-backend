b3933917fa2099eaa5ede8504ce2173cd5da9efb
Jakub <jakub.tomaszewskii100@gmail.com>
2026-01-07 21:50:13 +0100

    upgraded model version

--- utils/gemini.js
+++ utils/gemini.js
@@ -6,7 +6,7 @@ dotenv.config();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-1.5-pro-latest",
 });
 
 const PROMPT_TEXT = `
