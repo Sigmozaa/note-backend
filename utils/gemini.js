2204b40a00e17e4480b8b7c59e12d60990dfb7cb
Jakub <jakub.tomaszewskii100@gmail.com>
2026-01-07 22:03:27 +0100

    fixed error model name not found

--- utils/gemini.js
+++ utils/gemini.js
@@ -6,7 +6,7 @@ dotenv.config();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-pro-latest",
+  model: "gemini-1.5-pro-002",
 });
 
 const PROMPT_TEXT = `
