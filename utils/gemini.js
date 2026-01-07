20cbf923cc033c35bce7d9a45ea5e17c0ce236fa
Jakub <jakub.tomaszewskii100@gmail.com>
2026-01-07 22:07:01 +0100

    .

--- utils/gemini.js
+++ utils/gemini.js
@@ -6,7 +6,7 @@ dotenv.config();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-pro-002",
+  model: "gemini-1.5-pro",
 });
 
 const PROMPT_TEXT = `
